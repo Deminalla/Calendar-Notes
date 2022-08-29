ea0acf29d39e1acf36bf1c7813fd838650c2ba77
Deniza <elada.baltika@gmail.com>
2022-08-29 17:04:17 +0300

    note section update

--- src/main/java/com/example/calendarnotes/Controller.java
+++ src/main/java/com/example/calendarnotes/Controller.java
@@ -3,7 +3,6 @@ package com.example.calendarnotes;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -11,7 +10,6 @@ import javafx.scene.control.TextField;
 import java.time.Month;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ResourceBundle;
