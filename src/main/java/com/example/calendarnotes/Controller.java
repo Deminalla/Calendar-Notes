9d037d44cfbd9afe69c8848e047b8e4a64997a32
Deniza <elada.baltika@gmail.com>
2022-08-29 15:29:16 +0300

    year and month choiceboxes

--- src/main/java/com/example/calendarnotes/Controller.java
+++ src/main/java/com/example/calendarnotes/Controller.java
@@ -1,7 +1,32 @@
 package com.example.calendarnotes;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 
-public class Controller {
+import java.time.Month;
 
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private ChoiceBox<Integer> yearBox;
+
+    @FXML
+    private ChoiceBox<Month> monthBox;
+
+    Month[] monthList = Month.values();
+
+    List<Integer> yearList = IntStream.range(1970, 2023).boxed().collect(Collectors.toList());
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        monthBox.getItems().addAll(monthList);
+        yearBox.getItems().addAll(yearList);
+    }
 }
